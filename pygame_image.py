d26833b5c407cc7a8c0ef2aeca31c47afc1e1b7c
C0A22044 <c0a2204450@edu.teu.ac.jp>
2023-06-13 17:33:01 +0900

    test

--- pygame_image.py
+++ pygame_image.py
@@ -9,7 +9,10 @@ def main():
     bg_img = pg.image.load("ex01-20230613/fig/pg_bg.jpg")
     kk_img = pg.image.load("ex01-20230613/fig/3.png")
     kk_img = pg.transform.flip(kk_img, True,False)
-    kk_imgs = [kk_img,pg.transform.rotozoom(kk_img, 10, 1.0)]
+    kk_imgs=[]
+    for i in range(1, 11):
+        kk_imgs.append(pg.transform.rotozoom(kk_img, i, 1.0))
+     
     tmr = 0
     while True:
         for event in pg.event.get():
@@ -17,9 +20,12 @@ def main():
 
         screen.blit(bg_img,[-tmr,0])
         screen.blit(pg.transform.flip(bg_img, True,False),[1600-tmr,0])
-        screen.blit(kk_imgs[tmr%2],[300,200])
+        screen.blit(bg_img,[3200-tmr,0])
+        screen.blit(kk_imgs[tmr%10],[300,200])
         pg.display.update()
-        tmr += 1        
+        tmr += 1  
+        if tmr%3200==0:
+           tmr = 0      
 
         clock.tick(100)
 
